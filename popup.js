8c15d438d711f0be8a60588c6aee97546bca7489
k-maruono <ke.maruono@si.witc.co.jp>
2021-01-05 17:43:34 +0900

    コード整形

--- popup.js
+++ popup.js
@@ -7,39 +7,34 @@ $("#calc_time").on("click", () => {
 $("#set_time").on("click", () => {
   var cookie = readCookie();
   var ret = "";
-  if (cookie)
-  {
-  	ret = prompt("業務終了時刻を入力してください。(HHMM)", cookie);
+  if (cookie) {
+    ret = prompt("業務終了時刻を入力してください。(HHMM)", cookie);
   }
-  else
-  {
-  	ret = prompt("業務終了時刻を入力してください。(HHMM)");
+  else {
+    ret = prompt("業務終了時刻を入力してください。(HHMM)");
   }
 
-  if (ret)
-  {
-  	writeCookie(ret);
+  if (ret) {
+    writeCookie(ret);
   }
-  else
-  {
-  	alert("無効な値です。");
+  else {
+    alert("無効な値です。");
   }
 });
 
-function calc_time()
-{
+function calc_time() {
 };
 
 function readCookie() {
-      var tmp = document.cookie;
-      var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)calc_time_witc\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-      console.log(document.cookie);
-      return cookieValue;
+  var tmp = document.cookie;
+  var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)calc_time_witc\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  console.log(document.cookie);
+  return cookieValue;
 };
 
 function writeCookie(cookie) {
 
-      document.cookie = "calc_time_witc=" + cookie;
-      console.log(document.cookie);
+  document.cookie = "calc_time_witc=" + cookie;
+  console.log(document.cookie);
 
 };
